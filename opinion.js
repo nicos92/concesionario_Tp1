baf912c925fc564822e8151b8900f0ca8b532249
nicos92 <nicolassandoval140692@gmail.com>
2025-09-06 01:59:23 -0300

    feat: agrego seccion opiniones

---
+++ opinion.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const opinionItems = document.querySelectorAll('.opinion-item');
+
+    opinionItems.forEach((item) => {
+        let starCounter = 5; // Número de estrellas aleatorio entre 1 y 5
+
+        
+
+        const starElement = document.createElement('span');
+        starElement.classList.add('star-counter');
+
+        for (let i = 0; i < starCounter; i++) {
+            const starImg = document.createElement('img');
+            starImg.src = 'imgs/star.svg';
+            starElement.appendChild(starImg);
+        }
+
+        item.querySelector('.opinion-content').insertBefore(starElement, item.querySelector('.opinion-description'));
+    });
+});
